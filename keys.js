a4c81bcd0ad722562da0ff837df1c7e61b6f6334
Radsauce <radley.eakle@yahoo.com>
2018-10-03 21:42:06 -0700

    update keys.js

--- keys.js
+++ keys.js
@@ -4,3 +4,20 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+// spotify API
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: SPOTIFY_ID,
+  secret: SPOTIFY_SECRET,
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
+
